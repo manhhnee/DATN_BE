53e6b3d9bfd6261c97ff4521c3d366d167b8374d
Manh <manhddt2508@gmail.com>
2024-06-15 21:13:51 +0700

    add file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.hosts << "2hm-ai-store.click"
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
